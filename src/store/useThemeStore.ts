2f435a9af054c4d6d55b0c90e2db386d81c32218
Javad-code-stack <javadghafouri1999@gmail.com>
2025-08-26 21:01:24 +0330

    feat(theme): configure default and dark mode themes
    - Update index.css to set default theme to "nord" and dark mode to "sunset"
    - Change default theme in useThemeStore to "sunset"
    - Remove unused themes from daisyui configuration

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -1,13 +1,12 @@
 import { create } from "zustand";
 
-
 interface ThemeState {
 	theme: string;
 	setTheme: (theme: string) => void;
 }
 
 export const useThemeStore = create<ThemeState>((set) => ({
-	theme: localStorage.getItem("note-theme") || "dark",
+	theme: localStorage.getItem("note-theme") || "sunset",
 	setTheme: (theme: string) => {
 		localStorage.setItem("note-theme", theme);
 		set({ theme });
